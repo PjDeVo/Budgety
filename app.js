2ace5146af46c48ffd1c9cedca22d5e9d7925ffc
Owner <owner@Users-MacBook-Pro.local>
2019-10-07 21:40:45 -0400

    added modules for budget app and added event handler for add button. Listed necessary functionalty for add button as well

---
+++ app.js
@@ -0,0 +1,27 @@
+//Budget Controller
+
+var budgetController = (function () {
+    //Some Code
+
+})()
+
+//UI Controller
+var UIController = (function () {
+    //Some Code
+})();
+
+//Global App Controller 
+var controller = (function (budgetCtrl, UICtrl) {
+    document.querySelector('.add__btn').addEventListener('click', function () {
+        //1. Get the field Input Data
+
+        //2. Add the item to the budget controller.
+
+        //3. Add the new item to the UI.
+
+        //4. Calculate budget
+
+        //5. Display the budget to the UI
+    })
+
+})(budgetController, UIController);
