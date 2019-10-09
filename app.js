0fdba42b79ad85b95253d5a94f25c9506d6ddade
Owner <owner@Users-MacBook-Pro.local>
2019-10-09 00:32:19 -0400

    created the functionality that adds the input data to the storage in budgetController. var newItem in controller IIFE calls budget.addItem and receives input data from input variable that receives input from getInput method in UI Controller.

--- app.js
+++ app.js
@@ -1,5 +1,10 @@
 //Budget Controller
 
+
+
+
+
+
 var budgetController = (function () {
     //Some Code
     var Expense = function (id, description, value) {
@@ -29,6 +34,30 @@ var budgetController = (function () {
         totals: {
             exp: 0,
             inc: 0
+        }
+    }
+
+    return {
+        addItem: function (type, des, val) {
+            var newItem, ID;
+            if (data.allItems[type].length > 0) {
+                ID = data.allItems[type][data.allItems[type].length - 1] + 1
+            } else {
+                ID = 0;
+            }
+
+            if (type === 'exp') {
+                newItem = new Expense(ID, des, val);
+            } else if (type === 'inc') {
+                newItem = new Income(ID, des, val)
+            }
+
+            data.allItems[type].push(newItem);
+            return newItem;
+
+        },
+        testing: function () {
+            console.log(data);
         }
     }
 
@@ -65,10 +94,25 @@ var UIController = (function () {
 
 })();
 
+
+
 //Global App Controller 
 var controller = (function (budgetCtrl, UICtrl) {
     var getStrings = UICtrl.getDOMStrings();
 
+    var ctrlAddItem = function () {
+        var input, newItem;
+        //1. Get the field Input Data
+        var input = UICtrl.getInput();
+        console.log(input)
+        //2. Add the item to the budget controller.
+        var newItem = budgetCtrl.addItem(input.type, input.description, input.value);
+        //3. Add the new item to the UI.
+
+        //4. Calculate budget
+
+        //5. Display the budget to the UI
+    }
 
     var setUpEventListeners = function () {
         document.querySelector(getStrings.inputBtn).addEventListener('click', ctrlAddItem)
@@ -83,19 +127,6 @@ var controller = (function (budgetCtrl, UICtrl) {
     }
 
 
-    var ctrlAddItem = function () {
-        //1. Get the field Input Data
-        var input = UICtrl.getInput();
-        console.log(input)
-        //2. Add the item to the budget controller.
-
-        //3. Add the new item to the UI.
-
-        //4. Calculate budget
-
-        //5. Display the budget to the UI
-    }
-
     return {
         init: function () {
             console.log('Application has started')
@@ -107,3 +138,5 @@ var controller = (function (budgetCtrl, UICtrl) {
 })(budgetController, UIController);
 
 controller.init();
+
+
